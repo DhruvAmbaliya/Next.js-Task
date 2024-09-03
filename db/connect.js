df2891c59d3d41dc51a4d77c801151c7c63d04d6
DhruvAmbaliya <dhruvambaliya72@gmail.com>
2024-09-03 12:09:45 +0530

    fix : db connection

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 
 export const dbConnect = async () => {
   try {
-    const { connection } = await mongoose.connect(process.env.MONGO_URI);
+    const { connection } = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000,
+      connectTimeoutMS: 10000,
+    });
 
     if (connection.readyState === 1) {
       return Promise.resolve(true);
